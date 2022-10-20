eda91817e25a9407904bb12d661947c1978c6d46
Sandeep Kumar Patel <78929192+Ersandeep977@users.noreply.github.com>
2022-10-20 13:23:57 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,7 +8,12 @@ def mvn()
   sh 'mvn package'
 }
 
-def scp()
+def scp1()
 {
   sh 'scp /var/lib/jenkins/sa.txt ubuntu@172.31.20.2:/home/ubuntu'
+}
+
+def scp()
+{
+  sh 'scp /home/ubuntu/.jenkins/workspace/DeclarativePipeline3/webapp/target/webapp.war ubuntu@172.31.20.211:/var/lib/tomcat9/webapps/testapp.war'
 }
