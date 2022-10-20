ccccd000ffb6894f5e92bee79248936651a033cd
Sandeep Kumar Patel <78929192+Ersandeep977@users.noreply.github.com>
2022-10-20 08:08:39 +0530

    add git hub for function testing file

--- vars/github.groovy
+++ vars/github.groovy
@@ -1,4 +1,10 @@
-def gitcall()
+def gitmaven()
 {
   git 'https://github.com/Ersandeep977/DevOps-Maven-code.git'
 }
+
+def gitfunction()
+{
+ git 'https://github.com/Ersandeep977/DevOps-FunctionTesting-code.git'
+}
+
