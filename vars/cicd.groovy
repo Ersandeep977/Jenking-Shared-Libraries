fe5564b940cd562e16ea09a56fb40d8be0d152e7
Sandeep Kumar Patel <78929192+Ersandeep977@users.noreply.github.com>
2022-10-21 11:37:36 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -14,9 +14,16 @@ def scp1()
 }
 /*
 WORKSPACE=/var/lib/jenkins/workspace/Shared-Lib-3
+
+sh 'scp /home/ubuntu/.jenkins/workspace/DeclarativePipeline3/webapp/target/webapp.war ubuntu@172.31.20.211:/var/lib/tomcat9/webapps/testapp.war'
 */
 
 def scp(a)
 {
   sh 'scp ${a}/webapp/target/webapp.war ubuntu@172.31.20.2:/var/lib/tomcat9/webapps/webapp.war'
+}
+
+def scp1()
+{
+  sh 'scp /var/lib/jenkins/workspace/Shared-Lib-4/webapp/target/webapp.war ubuntu@172.31.20.2:/var/lib/tomcat9/webapps/testapp.war'
 }
