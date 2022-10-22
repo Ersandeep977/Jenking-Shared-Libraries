9df4ba870561cad70f5e11e006ca594f97b08395
Sandeep Kumar Patel <78929192+Ersandeep977@users.noreply.github.com>
2022-10-22 20:06:14 +0530

    Update github.groovy

--- vars/github.groovy
+++ vars/github.groovy
@@ -15,6 +15,7 @@ def MvnPack()
 
 def ScpData(WORKSPACE)
 {
+  sh "ssh ubuntu@172.31.20.2 'rm -rf /var/lib/tomcat9/webapps/testapp.war'"
   sh 'scp ${WORKSPACE}/webapp/target/webapp.war ubuntu@172.31.20.2:/var/lib/tomcat9/webapps/testapp.war'
 }
 
