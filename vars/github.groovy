0893dadd08c15b2bde511a09d947f1a064bedb78
Sandeep Kumar Patel <78929192+Ersandeep977@users.noreply.github.com>
2022-10-22 20:59:13 +0530

    Update github.groovy

--- vars/github.groovy
+++ vars/github.groovy
@@ -24,5 +24,11 @@ def SelTesting(WORKSPACE)
   sh 'java -jar ${WORKSPACE}/testing.jar'
 }
 
+def ScpData1(WORKSPACE)
+{
+  sh "ssh ubuntu@172.31.20.2 'rm -rf /var/lib/tomcat9/webapps/testapp.war'"
+  sh 'scp ${WORKSPACE}/webapp/target/webapp.war ubuntu@172.31.25.117:/var/lib/tomcat9/webapps/testapp.war'
+}
+
 
 
