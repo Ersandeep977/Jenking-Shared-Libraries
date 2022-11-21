b06ca891538032a2c36d695f380978630f2c3561
Sandeep Kumar Patel <78929192+Ersandeep977@users.noreply.github.com>
2022-11-21 18:28:24 +0530

    Update aws.groovy

--- vars/aws.groovy
+++ vars/aws.groovy
@@ -1,5 +1,8 @@
-withCredentials([[ 
+def aws(){
+               withCredentials([[ 
                 $class: 'AmazonWebServicesCredentialsBinding', 
                 credentialsId: 'AWS-Jenkins-Ec2-Full-acess', 
                 accessKeyVariable: 'AWS_ACCESS_KEY_ID', 
-                secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']])
+                secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+               ]])
+}
